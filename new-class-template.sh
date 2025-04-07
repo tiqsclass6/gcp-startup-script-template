4c0265770858ae4cd62ad0075e1adedaead187ae
tiqsclass6 <daquietstorm22@gmail.com>
2025-04-06 22:19:20 -0700

    update2 class-template.sh

--- new-class-template.sh
+++ new-class-template.sh
@@ -77,7 +77,7 @@ cat <<EOF > /var/www/html/index.html
     </div>
     <div class="w3-display-bottomleft w3-padding-small transparent-background outlined-text">
       <h1>Be a Man Level 4 - Brazil WebServer</h1>
-      <h3>I, TIQS, Thank You Theo and Beron</h3>
+      <h3>I, TIQS, Thank You Sensei Theo & Lord Beron</h3>
       <h3>Team Group Leader - John Sweeney</h3>
       <p><b>Instance Name:</b> $(hostname -f)</p>
       <p><b>Instance Private IP Address: </b> $local_ipv4</p>
