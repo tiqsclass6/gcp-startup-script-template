322ea5d8f5171b148884537251b1ec3685a6ac70
tiqsclass6 <daquietstorm22@gmail.com>
2025-04-06 22:15:35 -0700

    update1 class-template.sh

--- new-class-template.sh
+++ new-class-template.sh
@@ -67,7 +67,7 @@ cat <<EOF > /var/www/html/index.html
       </iframe>
       <hr class="w3-border-grey" style="margin:auto;width:40%;margin-top:15px;">
       <h3 class="w3-large w3-center" style="margin-top:15px;">
-        <a href="https://github.com/tiqsclass6/startup-script-template"
+        <a href="https://github.com/tiqsclass6/startup-script-template/blob/main/new-class-template.sh"
            class="w3-button w3-transparent w3-border w3-border-white w3-round-large w3-text-white"
            style="margin-bottom:0px;"
            target="_blank">
